b8ab8d029a21dcc32c2ee75899db01553e8f45d6
ByungchulJo <bc.cho@outlook.jp>
2025-05-25 15:36:15 +0900

    icon, navigation, koin-composeライブラリ追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,9 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.icon)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,6 +66,8 @@ dependencies {
     implementation(project.dependencies.platform(libs.koin.bom))
     implementation(libs.koin.core)
     implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+    implementation(libs.koin.compose.viewmodel)
 
 }
 
