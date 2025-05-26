877795d013c495ffc5c0846b45a0b1c26052c091
ByungchulJo <bc.cho@outlook.jp>
2025-05-27 02:36:27 +0900

    CardにPaddingを入れる

--- app/src/main/java/com/tryanything/myfavorite/ui/screen/MapScreen.kt
+++ app/src/main/java/com/tryanything/myfavorite/ui/screen/MapScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -83,7 +84,13 @@ private fun MapView(
         }
 
         selectedPlace.value?.also { selectedPlace ->
-            SelectedPlace(Modifier.align(Alignment.BottomCenter), selectedPlace, addToFavorite)
+            SelectedPlace(
+                Modifier
+                    .padding(horizontal = 15.dp)
+                    .align(Alignment.BottomCenter),
+                selectedPlace,
+                addToFavorite
+            )
         }
     }
 }
