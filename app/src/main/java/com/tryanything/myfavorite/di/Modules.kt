9895d5a9938ee55b2065a81be82421e924740fb9
ByungchulJo <bc.cho@outlook.jp>
2025-05-27 23:34:35 +0900

    検索などの機能をDispatcher.IOで実行する

--- app/src/main/java/com/tryanything/myfavorite/di/Modules.kt
+++ app/src/main/java/com/tryanything/myfavorite/di/Modules.kt
@@ -2,8 +2,14 @@ package com.tryanything.myfavorite.di
 
 import com.tryanything.myfavorite.ui.favorite.FavoriteViewModel
 import com.tryanything.myfavorite.ui.screen.MapViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
+
+val dispatcher = module {
+    single<CoroutineDispatcher> { Dispatchers.IO }
+}
 
 val viewModels = module {
     viewModelOf(::FavoriteViewModel)
--- app/src/main/java/com/tryanything/myfavorite/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/tryanything/myfavorite/ui/favorite/FavoriteViewModel.kt
@@ -4,18 +4,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tryanything.myfavorite.model.Place
 import com.tryanything.myfavorites.repository.FavoriteRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class FavoriteViewModel(
-    private val repository: FavoriteRepository
+    repository: FavoriteRepository,
+    dispatcher: CoroutineDispatcher // Test実装のため
 ) : ViewModel() {
 
     val favorites: StateFlow<List<Place>> = repository
         .observeAllFavorites()
         .map { list -> list.map { Place(it) } }
+        .flowOn(dispatcher)
         .stateIn(
             viewModelScope,
             SharingStarted.Eagerly,
